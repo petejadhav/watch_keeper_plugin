24bee62896c505f5d97a0245f63e2966b9b54276
Prithviraj Jadhav - Ubuntu Desktop <prithviraj.en@gmail.com>
2021-08-29 01:00:41 +0530

    added replace,remove functions

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
 chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ 'watchkeeper_lists':['watch_list'] });
   chrome.storage.sync.set({ 'watch_list':[{'date':'2021-08-01', 'url':'https://qwe.com', 'title':'qwe'}] });
   console.log('Initialized empty watch list');
 });
--- popup.js
+++ popup.js
@@ -1,11 +1,51 @@
 function displayListItem(title, url, date) {
     var ul = document.getElementById("watch_list");
+    var li_str = `<a href='{url}'>{title} - {date}</a> <button class='{title}' id="replace-btn">Replace</button> <button class='{title}' id="remove-btn">Remove</button>`
+    li_str = li_str.replaceAll('{url}',url).replaceAll('{title}',title).replaceAll('{date}',date)
     var li = document.createElement("li");
-    var link = document.createElement("a");
-    link.setAttribute('href', url)
-    link.appendChild(document.createTextNode(title + " - " + date));
-    li.appendChild(link);
+    li.setAttribute('id',title);
+    li.innerHTML = li_str;
     ul.appendChild(li);
+}
+
+async function replace(e){
+    console.log(e);
+    var className = e.target.className
+
+    document.getElementById(className).remove();
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    chrome.storage.sync.get(['watch_list'], function(result) {
+        var list = result['watch_list']
+        console.log(list);
+        var newObj = {'date':new Date().toISOString().slice(0, 10), 'url':tab.url, 'title':tab.title}
+        list[list.findIndex(obj => obj.title == className)] = newObj
+        chrome.storage.sync.set({ 'watch_list': list });
+        displayListItem(newObj['title'], newObj['url'], newObj['date']);
+    });
+    
+    console.log("removed")
+
+}
+
+function remove(e){
+    console.log(e)
+    var className = e.target.className
+
+    document.getElementById(className).remove();
+
+    chrome.storage.sync.get(['watch_list'], function(result) {
+        var list = result['watch_list']
+        console.log(list);
+        list.splice(list.findIndex(obj => obj.title == className),1)
+        chrome.storage.sync.set({ 'watch_list': list });
+    });
+
+    console.log("removed")
+}
+
+function createNewList(name){
+
 }
 
 function appendWatchList(title, url, date){
@@ -17,7 +57,6 @@ function appendWatchList(title, url, date){
         displayListItem(title, url, date);
     });
 }
-
 
 window.onload = () => {
     console.log("popup loaded");
@@ -38,3 +77,16 @@ document.getElementById("get_title").addEventListener("click", async (e) => {
     //displayListItem(tab.title, tab.url, "2021-01-01");
     appendWatchList(tab.title, tab.url, new Date().toISOString().slice(0, 10));
 })
+
+document.body.addEventListener('click', async (event) => {
+    const isButton = event.target.nodeName === 'BUTTON';
+    if (!isButton) {
+      return;
+    }
+  
+    if(event.target.id=='replace-btn')
+        replace(event);
+    else if(event.target.id=='remove-btn')
+        remove(event);
+  })
+  
